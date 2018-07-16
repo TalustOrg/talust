512e258818a1d3172231f596597539eeea928daa
Axeliu <904198604@qq.com>
2018-07-16 17:51:50 +0800

    update method for GEN tool , for account security

--- account/src/main/java/org/talust/chain/account/CAGenTool.java
+++ account/src/main/java/org/talust/chain/account/CAGenTool.java
@@ -34,11 +34,6 @@ import java.math.BigInteger;
 public class CAGenTool {
 
     public static void main(String[] args) {
-//        ECKey rootKey = genRootCa();
-//        ECKey genesisKey = genGenesisCa();
-//        signGenesisCa(rootKey,genesisKey);
-
-
         ECKey ecKey = new ECKey();
         String privateKeyAsHex = ecKey.getPrivateKeyAsHex();
         String publicKeyAsHex = ecKey.getPublicKeyAsHex();
@@ -52,36 +47,6 @@ public class CAGenTool {
     public static byte[] signPub(ECKey parentKey,ECKey selfKey){
         return parentKey.sign(selfKey.getPubKey());
     }
-
-//
-//    //生成根证书
-//    private static ECKey genRootCa() {
-//        //设置两重密码主要是为了安全
-//        String password1 = "root.password1";//根用户密码1
-//        String password2 = "root.password2";//根用户密码2
-//        ECKey ecKey1 = genECKeyByPassword(password1);
-//        ECKey ecKey2 = genECKeyByPassword(password2);
-////        BigInteger privKey = genPrivKey(ecKey1.getPubKey(false), ecKey2.getPubKey(false));
-////        ECKey ecKey = ECKey.fromPrivate(privKey);
-//        //String publicKeyAsHex = ecKey.getPublicKeyAsHex();
-//        //System.out.println(publicKeyAsHex);
-//        //03fecab6625b15d40d3245a17fd1a367ed1c17511e7f665b6a951d5b470deb200e   //这是根证书的公钥
-//        return ecKey;
-//    }
-
-//    //生成创世块公钥
-//    private static ECKey genGenesisCa() {
-//        String password1 = "genesis.password1";//创世块帐户密码1
-//        String password2 = "genesis.password2";//创世块帐户密码2
-//        ECKey ecKey1 = genECKeyByPassword(password1);
-//        ECKey ecKey2 = genECKeyByPassword(password2);
-//        BigInteger privKey = genPrivKey(ecKey1.getPubKey(false), ecKey2.getPubKey(false));
-//        ECKey ecKey = ECKey.fromPrivate(privKey);
-//        String publicKeyAsHex = ecKey.getPublicKeyAsHex();
-//        System.out.println(publicKeyAsHex);
-//        //029736b6e388cb44f436a5531474c294eced77c368ea2ed5638a97665c0ecf279c   //这是创世块帐户的公钥
-//        return ecKey;
-//    }
 
 
     //根据密码生成ECKey对象
