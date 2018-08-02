7453862b7fedfce3a2773c29d45119b53867d91d
Axe-Liu <904198604@qq.com>
2018-08-02 09:17:16 +0800

    NTP  time server , using method

---
+++ common/src/main/java/org/talust/common/tools/NtpTimeService.java
@@ -0,0 +1,146 @@
+package org.talust.common.tools;/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 talust.org talust.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import org.apache.commons.net.ntp.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.text.NumberFormat;
+
+/**
+ * @author Axe-Liu
+ * @date 2018/8/1.
+ */
+public class NtpTimeService {
+    private static final NumberFormat NUM_FORMAT = new java.text.DecimalFormat("0.00");
+    private static final String SERVER_IP = "60.205.205.64";
+    public static void main(String[] args) throws IOException {
+        NTPUDPClient client = new NTPUDPClient();
+        // We want to timeout if a response takes longer than 10 seconds
+        client.setDefaultTimeout(10000);
+        try {
+            client.open();
+            InetAddress hostAddr = InetAddress.getByName(SERVER_IP);
+            System.out.println(" > " + hostAddr.getHostName() + "/" + hostAddr.getHostAddress());
+            TimeInfo info = client.getTime(hostAddr);
+            processResponse(info);
+        } catch (SocketException e) {
+            e.printStackTrace();
+
+        }
+        client.close();
+
+    }
+
+    private static void processResponse(TimeInfo info) {
+        NtpV3Packet message = info.getMessage();
+        int stratum = message.getStratum();
+        String refType;
+        if (stratum <= 0){
+            refType = "(Unspecified or Unavailable)";
+        }
+        else if (stratum == 1){
+            // GPS, radio clock, etc.
+            refType = "(Primary Reference; e.g., GPS)";
+        }
+        else{
+            refType = "(Secondary Reference; e.g. via NTP or SNTP)";
+        }
+        // stratum should be 0..15...
+        System.out.println(" Stratum: " + stratum + " " + refType);
+        int version = message.getVersion();
+        int li = message.getLeapIndicator();
+        System.out.println(" leap=" + li + ", version="
+                + version + ", precision=" + message.getPrecision());
+        System.out.println(" mode: " + message.getModeName() + " (" + message.getMode() + ")");
+        int poll = message.getPoll();
+        // poll value typically btwn MINPOLL (4) and MAXPOLL (14)
+        System.out.println(" poll: " + (poll <= 0 ? 1 : (int) Math.pow(2, poll))
+                + " seconds" + " (2 ** " + poll + ")");
+        double disp = message.getRootDispersionInMillisDouble();
+        System.out.println(" rootdelay=" + NUM_FORMAT.format(message.getRootDelayInMillisDouble())
+                + ", rootdispersion(ms): " + NUM_FORMAT.format(disp));
+        int refId = message.getReferenceId();
+        String refAddr = NtpUtils.getHostAddress(refId);
+        String refName = null;
+        if (refId != 0) {
+            if (refAddr.equals("127.127.1.0")) {
+                // This is the ref address for the Local Clock
+                refName = "LOCAL";
+            } else if (stratum >= 2) {
+                // If reference id has 127.127 prefix then it uses its own reference clock
+                // defined in the form 127.127.clock-type.unit-num (e.g. 127.127.8.0 mode 5
+                // for GENERIC DCF77 AM; see refclock.htm from the NTP software distribution.
+                if (!refAddr.startsWith("127.127")) {
+                    try {
+                        InetAddress addr = InetAddress.getByName(refAddr);
+                        String name = addr.getHostName();
+                        if (name != null && !name.equals(refAddr)){
+                            refName = name;
+                        }
+                    } catch (UnknownHostException e) {
+                        // some stratum-2 servers sync to ref clock device but fudge stratum level higher... (e.g. 2)
+                        // ref not valid host maybe it's a reference clock name?
+                        // otherwise just show the ref IP address.
+                        refName = NtpUtils.getReferenceClock(message);
+                    }
+                }
+            } else if (version >= 3 && (stratum == 0 || stratum == 1)) {
+                refName = NtpUtils.getReferenceClock(message);
+                // refname usually have at least 3 characters (e.g. GPS, WWV, LCL, etc.)
+            }
+            // otherwise give up on naming the beast...
+        }
+        if (refName != null && refName.length() > 1){
+            refAddr += " (" + refName + ")";
+        }
+        System.out.println(" Reference Identifier:\t" + refAddr);
+        TimeStamp refNtpTime = message.getReferenceTimeStamp();
+        System.out.println(" Reference Timestamp:\t" + refNtpTime + "  " + refNtpTime.toDateString());
+        // Originate Time is time request sent by client (t1)
+        TimeStamp origNtpTime = message.getOriginateTimeStamp();
+        System.out.println(" Originate Timestamp:\t" + origNtpTime + "  " + origNtpTime.toDateString());
+        long destTime = info.getReturnTime();
+        // Receive Time is time request received by server (t2)
+        TimeStamp rcvNtpTime = message.getReceiveTimeStamp();
+        System.out.println(" Receive Timestamp:\t" + rcvNtpTime + "  " + rcvNtpTime.toDateString());
+        // Transmit time is time reply sent by server (t3)
+        TimeStamp xmitNtpTime = message.getTransmitTimeStamp();
+        System.out.println(" Transmit Timestamp:\t" + xmitNtpTime + "  " + xmitNtpTime.toDateString());
+        // Destination time is time reply received by client (t4)
+        TimeStamp destNtpTime = TimeStamp.getNtpTime(destTime);
+        System.out.println(" Destination Timestamp:\t" + destNtpTime + "  " + destNtpTime.toDateString());
+        info.computeDetails(); // compute offset/delay if not already done
+        Long offsetValue = info.getOffset();
+        Long delayValue = info.getDelay();
+        String delay = (delayValue == null) ? "N/A" : delayValue.toString();
+        String offset = (offsetValue == null) ? "N/A" : offsetValue.toString();
+        // offset in ms
+        System.out.println(" Roundtrip delay(ms)=" + delay
+                + ", clock offset(ms)=" + offset);
+    }
+}
