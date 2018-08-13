696e0edd38174ace4347d05f161b1f2441cd83b3
Axeliu <904198604@qq.com>
2018-08-13 13:40:10 +0800

    creator account method modify , account verify , default account login , account file construction changed

---
+++ network/src/main/java/org/talust/network/handle/AbstractTimeoutHandler.java
@@ -0,0 +1,39 @@
+package org.talust.network.handle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public abstract class AbstractTimeoutHandler {
+	
+    private TimerTask timeoutTask;
+    private long timeoutMillis = 0;
+    private boolean timeoutEnabled = true;
+
+    private static final Timer timeoutTimer = new Timer("AbstractTimeoutHandler timeouts", true);
+
+    public synchronized final void setTimeoutEnabled(boolean timeoutEnabled) {
+        this.timeoutEnabled = timeoutEnabled;
+        resetTimeout();
+    }
+
+    public synchronized final void setSocketTimeout(int timeoutMillis) {
+        this.timeoutMillis = timeoutMillis;
+        resetTimeout();
+    }
+
+    protected synchronized void resetTimeout() {
+        if (timeoutTask != null)
+            timeoutTask.cancel();
+        if (timeoutMillis == 0 || !timeoutEnabled)
+            return;
+        timeoutTask = new TimerTask() {
+            @Override
+            public void run() {
+                timeoutOccurred();
+            }
+        };
+        timeoutTimer.schedule(timeoutTask, timeoutMillis);
+    }
+
+    protected abstract void timeoutOccurred();
+}
