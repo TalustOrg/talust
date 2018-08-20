578eadc4b7d94b307eedaaf88933b35a930d061d
Axeliu <904198604@qq.com>
2018-08-20 13:11:15 +0800

    fix thrift address store public class and invocation method.

---
+++ common/src/main/java/org/talust/common/model/Deposits.java
@@ -0,0 +1,41 @@
+package org.talust.common.model;/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 talust.org talust.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import io.protostuff.Tag;
+
+import java.util.List;
+
+public class Deposits {
+    @Tag(1)//帐户地址
+  private List<DepositAccount> depositAccounts;
+
+    public List<DepositAccount> getDepositAccounts() {
+        return depositAccounts;
+    }
+
+    public void setDepositAccounts(List<DepositAccount> depositAccounts) {
+        this.depositAccounts = depositAccounts;
+    }
+}
